4647ec841821735363fa24a4116f9ccf3d5d2ec0
kreel83 <marc.borgna@gmail.com>
2020-04-02 11:20:41 +0200

    nouvelle recherche

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,91 @@
-const modal =() => {
+const modal = () => {
     $('.card').click(() => {
         $('.modal').modal('toggle')
     })
 }
 
-export {modal}
+
+const navItems = () => {
+    $('.nav-pills li').click(function () {
+        const id = $(this).attr('id')
+        $('.tab-content div').removeClass('active show')
+        $(`.tab-content div[data-id="${id}"]`).addClass('active show')
+    })
+}
+
+
+const searchLivreAddict = () => {
+    $('#pagination li').click(function () {
+        let page = $(this).attr('data-position')
+        const titre = $('#titre').val()
+        const nbpage = $('.pagination li').length - 2
+        const pageActuelle = parseInt($('.pagination li.active').attr('data-position'))
+        console.log(pageActuelle)
+        if (page == 1) {
+            $('.pagination').find("li:first").attr('hidden', true)
+        } else {
+            $('.pagination').find("li:first").attr('hidden', false)
+        }
+        if (page == nbpage) {
+            $('.pagination').find("li:last").attr('hidden', true)
+        } else {
+            $('.pagination').find("li:last").attr('hidden', false)
+        }
+        if (page == 'next') {
+            page = pageActuelle + 1
+            if (page == nbpage) {
+                $('.pagination').find("li:last").attr('hidden', true)
+            }
+
+        }
+        if (page == 'previous') {
+            page = pageActuelle - 1
+            if (page == 1) {
+                $('.pagination').find("li:first").attr('hidden', true)
+            }
+        }
+
+
+        $.get({
+            url: '/google/rechercheLivreaddict/' + titre + '/' + page
+        })
+            .done((data) => {
+                    const books = JSON.parse(data);
+
+                    $('.content').empty()
+
+
+
+
+
+                    for (let i = 0; i < books.length; i++) {
+                        const book = `<div class="card"
+                         style="width: 200px;font-size: 0.8rem;margin: 10px;padding: 0; cursor:pointer;">
+                        <div class="card-body d-flex justify-content-center">
+                            <img src="${books[i]['photo']}" alt="" width="80" height="125">
+                        </div>
+                        <div class="card-footer">
+                            ${books[i]['lien']}
+                        </div>
+                        <div class="card-footer">
+                            <p>${books[i]['genre']}</p>
+                            [<em>
+                            ${books[i]['auteur']}
+
+                        </em>]
+                        </div>
+                    </div> `
+                        $('.content').append(book)
+                        $('.pagination li').removeClass('active')
+                        $(`.pagination li:eq(${page})`).addClass('active')
+
+
+                    }
+
+
+                }
+            )
+    })
+}
+
+export {modal, searchLivreAddict, navItems}
--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -3,12 +3,12 @@
 namespace App\Controller;
 
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use JBarbin\GoogleBooksBundle\GoogleAPI\Query;
+use Goutte\Client;
 
 class GoogleController extends AbstractController
 {
@@ -16,7 +16,8 @@ class GoogleController extends AbstractController
      * @Route ("/accueil", name="accueil")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function test() {
+    public function test()
+    {
         return $this->render('accueil.html.twig');
     }
 
@@ -24,12 +25,13 @@ class GoogleController extends AbstractController
      * @Route ("google/ajax", name="ajax")
      * @return string
      */
-    public function ajaxRender() {
+    public function ajaxRender()
+    {
         $b = ["title" => "titre", "author" => "auteur", "description" => "description"];
         $query = "q=autant";
         $books = $this->recherhe($query);
         $details = [];
-        foreach ($books->items as $key=>$book) {
+        foreach ($books->items as $key => $book) {
             $det = array();
             $det['titre'] = $book->volumeInfo->title;
             isset($book->volumeInfo->authors[0]) ? $det['author'] = $book->volumeInfo->authors[0] : $det['author'] = "";
@@ -42,36 +44,106 @@ class GoogleController extends AbstractController
     }
 
 
-
-    private function recherhe($q) {
+    private function recherche($q)
+    {
         $url = "https://www.googleapis.com/books/v1/volumes?$q&printType=books&projection=lite&maxResults=40&langRestrict=Fr";
-        dump($url);
+
         $b = file_get_contents($url);
         return json_decode($b);
     }
 
 
+    private function nb($p)
+    {
+        $client = new Client();
+        $url = "https://www.livraddict.com/search.php?t=$p";
+        $crawler = $client->request('GET', $url);
+        $rr = $crawler->filter('.listing_recherche>li')->last()->extract("_text");
+        dump($rr);
+        if ($rr == []) {
+            return 99;
+        }
+        return intval($rr[0]);
+    }
+
+    /**
+     * @Route("/google/rechercheLivreaddict/{titre}/{lapage}", name="rechercheLivraddict")
+     * @param $titre , $lapage
+     * @return array
+     */
+    public function rechercheLivreaddict($titre, $lapage)
+    {
+        $client = new Client();
+        $r = [];
+        $url = "https://www.livraddict.com/search.php?page=$lapage&t=$titre";
+        $crawler = $client->request('GET', $url);
+        $rr = $crawler->filter('.listing_recherche>li');
+        $r['lien'] = $rr->filter('div>a')->extract(array('href'));
+        $r['photo'] = $rr->filter('div>a>img')->extract(array('src'));
+        $r['auteur'] = $rr->filter('.item_infos>p>a')->extract("_text");
+        $r['genre'] = $rr->filter('.item_infos>.genre')->extract("_text");
+
+
+        $books = [];
+
+        $nb = sizeof($r['lien']);
+        for ($i = 0; $i < $nb; $i++) {
+            $books[$i]['lien'] = $r['lien'][$i];
+            $books[$i]['photo'] = $r['photo'][$i];
+            $books[$i]['auteur'] = $r['auteur'][$i];
+            $books[$i]['genre'] = $r['genre'][$i];
+        }
+
+        $authors = [];
+        $auth = $crawler->filter('#tab_auteurs>ul');
+        $a['nom'] = $auth->filter("li>a")->extract(array("_text"));
+        $a['link'] = $auth->filter("li>a")->extract(array("href"));
+        $nb = sizeof($a['nom']);
+        for ($i = 0; $i < $nb; $i++) {
+            $authors[$i]['nom'] = $a['nom'][$i];
+            $authors[$i]['link'] = $a['link'][$i];
+        }
+
+        dump($authors);
+        return new Response(json_encode(["books" => $books, "authors" => $authors]));
+    }
+
     /**
      * @Route("/google/search", name="google_search")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function search(Request $request) {
+    public function search(Request $request)
+    {
         $books = [];
         $r = [];
         if (sizeof($request->request) > 0) {
-            $r['titre'] = $request->request->get('titre');
-            $r['auteur']=  $request->request->get('auteur');
+            $r['titre'] = trim(str_replace(' ', '+', $request->request->get('titre')));
+            $r['auteur'] = $request->request->get('auteur');
             if ($request->request->get('titre') != "") {
-                $urlParams = "q=".str_replace(' ', '+', trim($request->request->get('titre')));
+                $urlParams = "q=" . str_replace(' ', '+', trim($request->request->get('titre')));
             } else {
-                $urlParams='q=';
+                $urlParams = 'q=';
             }
             if ($request->request->get('auteur') != "") {
-                $urlParams .= '+inauthor:'.str_replace(' ', '+', trim($request->request->get('auteur')));
+                $urlParams .= '+inauthor:' . str_replace(' ', '+', trim($request->request->get('auteur')));
             }
-            $books = $this->recherhe($urlParams);
+            if ($request->request->get('valide') == "google") {
+                $books = $this->recherche($urlParams);
+            } else {
 
+                $books = $this->rechercheLivreaddict($r['titre'], 1);
+                $nb = $this->nb($r['titre']);
+                $books = json_decode($books->getContent());
+                dump($books);
+                return $this->render('google/livreAddict.html.twig', [
+                    'books' => $books->books,
+                    'authors' => $books->authors,
+                    'nbpage' => $nb,
+                    'request' => $r
+                ]);
+            }
         }
+        dump($books);
         return $this->render('google/search.html.twig', [
             'books' => $books,
             'request' => $r
@@ -85,7 +157,7 @@ class GoogleController extends AbstractController
     public function index()
     {
         $query = "q=autant";
-        $books = $this->recherhe($query);
+        $books = $this->recherche($query);
 
         return $this->render('google/index.html.twig', [
             'controller_name' => 'GoogleController',
